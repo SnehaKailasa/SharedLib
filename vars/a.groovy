126721df1c4531c3c1a5d2f8a49a3f071b703bd6
SnehaKailasa <sneha.kailasa@ggktech.com>
2018-01-02 11:14:14 +0530

    Update a.groovy

--- vars/a.groovy
+++ vars/a.groovy
@@ -1,17 +1,24 @@
+class A {
+	def lock_resource_name = "jenkins-file"
+         def Sonar_project_name = "jenkins-file"
+}
 def call() {
-  echo "Hello"
-  
-   stage ('Tests') {
-                    parallel 'static': {
-                        sh "echo 'shell scripts to run static tests...'"
-                    },
-                    'unit': {
-                        sh "echo 'shell scripts to run unit tests...'"
-                    },
-                    'integration': {
-                        sh "echo 'shell scripts to run integration tests...'"
-                    }
-                }
-  
-  
+JobName = "${JOB_NAME}"
+	A a = new A()
+	
+           a.lock_resource_name = "jenkins-file"
+           a.Sonar_project_name = "jenkins-file"
+	println a.lock_resource_name
+           println a.Sonar_project_name
+	return a
+	/*	if("${BRANCH_NAME}".startsWith('PR-'))
+			{
+             	lock_resource_name = JobName.substring(0 , JobName.indexOf("/"))+"_"+"${CHANGE_TARGET}"
+                Sonar_project_name = lock_resource_name + "PR"
+			}
+			else
+			{
+				 lock_resource_name = JobName.substring(0 , JobName.indexOf("/"))+"_"+"${BRANCH_NAME}"
+				 Sonar_project_name = lock_resource_name
+			} */
 }
